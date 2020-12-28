d88d5f857ef9cc9575939107c55cbecbae93709a
Yura Shostenko <72665160+YShos@users.noreply.github.com>
2020-12-28 04:51:02 +0300

    Add files via upload

---
+++ zachet3.py
@@ -0,0 +1,5 @@
+#Программный код для получения номера машины с фотографии
+#https://web-answers.ru/c/python-raspoznavat-simvoly-nomernogo-znaka.html
+#статья про распознавание номеров машин
+#https://habr.com/ru/company/recognitor/blog/225913/
+
